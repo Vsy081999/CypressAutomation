e099f41813631caa7c1753c91b777aa296963377
Vishwanath Yadav <Vsy081999@gmail.com>
2024-04-30 22:37:31 +0530

    first cypress commit

---
+++ cypress/e2e/1-getting-started/Test.cy.js
@@ -0,0 +1,8 @@
+
+describe('login ',()=>{
+    it('login saucelabs',()=>{
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+cy.contains("Mouse Hover").scrollIntoView();
+    })
+})
